b8d7c86aa6a4031cf5d4c0487a6e9c70c417d29c
Ailsa <ailsaacjohnston@gmail.com>
2022-09-25 11:39:58 +0100

    add function getComputerChoice

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,1 +1,14 @@
 console.log ("Hello World");
+// User with enter Rock, Paper or Scissors
+// When entering their choice, the computer will also make a choice 
+// A result will be drawn from answers 
+
+function getComputerChoice () {  
+    
+    if (Math.floor(Math.random()*3) === 1) { 
+        console.log ("Rock");
+    } else if (Math.floor(Math.random()*3) === 2) { 
+        console.log ("Paper");
+    } else  (Math.floor(Math.random()*3) === 3) {
+        console.log ("Scissors");}
+}; 
