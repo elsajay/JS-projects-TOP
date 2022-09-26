18903d463556c264f28fac72d926947d801d3262
Ailsa <ailsaacjohnston@gmail.com>
2022-09-26 17:27:58 +0100

    add notes to code, add playerSelection function. Determine winner varibale/ function to be finalised

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,14 +1,48 @@
-console.log ("Hello World");
+// console.log ("Hello World");
 // User with enter Rock, Paper or Scissors
 // When entering their choice, the computer will also make a choice 
 // A result will be drawn from answers 
 
+let computerSelection;
+
+// Function getComputerChoice randomly selects input for Rock Paper Scissors 
 function getComputerChoice () {  
-    
-    if (Math.floor(Math.random()*3) === 1) { 
-        console.log ("Rock");
-    } else if (Math.floor(Math.random()*3) === 2) { 
-        console.log ("Paper");
-    } else  (Math.floor(Math.random()*3) === 3) {
-        console.log ("Scissors");}
-}; 
+    if (Math.floor(Math.random()*3) === 0) { 
+        return (computerSelection = "rock");
+    } else if (Math.floor(Math.random()*3) === 1) { 
+        return (computerSelection = "paper");
+    } else {
+        return (computerSelection = "scissors");
+    } 
+};
+
+computerSelection = getComputerChoice();
+
+let playerSelection = prompt("Do you choose rock, paper or scissors?")
+playerSelection = playerSelection.toLowerCase();  // This converts choice to all lower case.
+
+// This section prints what the player choice is. 
+
+if (playerSelection != null) {
+    console.log (`You chose ${playerSelection}.`)
+}
+
+// This section tells you what the player + computer choices are. 
+
+if (playerSelection != null && computerSelection != null) { 
+    console.log (`The computer chose ${computerSelection}.`)
+}
+let winnerIs; // variable for the winner 
+
+
+// Function to determine the winner (uncomplete - to be finished - doesn't run correctly yet). 
+function round (playerSelection, computerSelection) { 
+
+    if (playerSelection === "rock" + computerSelection === "paper" ) { 
+        return (winnerIs = "Player wins")
+     } else { 
+        return (winnerIs = "This was a draw. Try again to win!")
+    }
+
+}
+console.log (`Winner? ${winnerIs}`);
